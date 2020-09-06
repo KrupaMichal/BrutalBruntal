4e5882bad1b2e65c9cd1a494fa8ed29b81e0a062
KrupaMichal <krupa2002@outlook.cz>
2020-09-06 19:33:55 +0200

    V1.4
    Přidání Boltu do projektu + textura dvěří + skript  a animace dvěří

---
+++ Assets/Skripty/Prostredi/Dvere/DvereOt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DvereOt : MonoBehaviour
+{
+
+        public GameObject dvere;
+
+        void OnTriggerEnter(Collider other) {
+            dvere.GetComponent<Animator>().Play("DvereOby");
+        }
+}
